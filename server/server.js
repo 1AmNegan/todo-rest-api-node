594bfb8e11c3ff4a0df17dc975f9ca7666ee537c
1AmNegan <muralisribalaji@gmail.com>
2017-10-01 19:41:36 +0530

    Add POST /todos route

--- server/server.js
+++ server/server.js
@@ -1,3 +1,25 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+
 const { mongoose } = require('./db/mongoose');
 const { Todo } = require('./models/todo');
 const { User } = require('./models/user');
+
+const app = express();
+
+app.use(bodyParser.json());
+
+app.post('/todos', (req, res) => {
+  var todo = new Todo({
+    text: req.body.text
+  });
+  todo.save().then((doc) =>{
+    res.send(doc);
+  }).catch((e) => {
+    res.status(400).send(e);
+  });
+})
+
+app.listen(3000, () =>{
+  console.log('Started on port 3000');
+});
