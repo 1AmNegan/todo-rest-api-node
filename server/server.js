886026e10f5acaa4f8af88e143ad250bd2d27de3
1AmNegan <muralisribalaji@gmail.com>
2017-10-01 18:55:18 +0530

    add User model

--- server/server.js
+++ server/server.js
@@ -28,3 +28,22 @@ newTodo.save().then((doc) => {
 }, (e) => {
   console.log('Unable to save');
 });
+
+var User = mongoose.model('User', {
+  email: {
+    type: String,
+    required: true,
+    minlength: 1,
+    trim: true
+  }
+});
+
+var newUser = new User({
+  email: 'newUser@example.com'
+});
+
+newUser.save().then((doc) => {
+  console.log('Saved Todo', doc);
+}, (e) => {
+  console.log('Unable to save');
+});
